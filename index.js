63485a2f1a2112f82a22dcf753bb22875ce94977
nabilaba <nabilazizbimaanggita@gmail.com>
2022-12-20 13:03:43 +0700

    fix cors

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const db = require("./models");
 const port = 8000;
 const carsRoute = require("./routes/cars.routes");
 const { seedingCars } = require("./utils/seeding");
+const cors = require("cors");
 
 db.sequelize.sync();
 
 seedingCars(db.sequelize);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", express.static("public"));
 app.use("/uploads", express.static("uploads"));
